545b9be38ccbe80a947c86d74c0f7452b8786e5e
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-02-18 23:40:20 +0530

    Event Binding creat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'angular';
+  onclick(){
+    console.log('hello word in anguler creat event binding')
+  }
+  ahow(){
+    alert('hello event binding')
+  }
 }
