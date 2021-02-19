d5469ccc80d67ce1f9d595241420515ad2553246
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-02-19 11:04:29 +0530

    ROUTING in link and button and component throw dono shide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { StudentdetailComponent } from './studentdetail/studentdetail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'page1' ,component : StudentComponent},
+  {path:'page2' ,component : StudentdetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
