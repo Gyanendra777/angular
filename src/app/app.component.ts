5e2ee711d02f98a2ce7d163be6256ab0c4f0dfb0
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-02-20 10:10:42 +0530

    http client not rune

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  public persondata = [];
+  constructor (private myservice:MyserviceService){}
+  ngOnInit(){
+    this.myservice.getData().subscribe((data) => {
+      this.persondata = Array.from(Object.keys(data),k=>data[k] );
+      console.log(this.persondata);
+    });
+  }
 }
