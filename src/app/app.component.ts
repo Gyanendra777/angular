7433c0885e70515b64d7a9d5a132d1f1e5c85158
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-02-20 19:18:52 +0530

    api 2 tip cole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserServiceService } from './user-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  constructor(private user:UserServiceService){
+    // fist tip api call 
+    this.user.getData().subscribe(data=>{
+      console.warn(data)
+    })
+
+  }
+  ngOnInit(){
+    // secend tip api call 
+    this.user.getData().subscribe(data=>{
+      console.warn("resp",data)
+    })
+
+  }
 }
