bab7c1d2fe2b607e88c8baf340a57d708c53a706
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-08 21:05:38 +0530

    output  passing data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { StudentComponent } from './student/student.component';
-import { StudentdetailComponent } from './studentdetail/studentdetail.component';
+
 
 const routes: Routes = [
-  // {path:'',redirectTo:'page1',pathMatch:'full'},
-  {path:'page1' ,component : StudentComponent},
-  {path:'page2' ,component : StudentdetailComponent},
-  // {path:'**' ,component : PagenotfoundComponent},
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { StudentdetailComponent } from './studentdetail/studentdetail.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
-    StudentdetailComponent,
-    PagenotfoundComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
